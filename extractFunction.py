3797e02c1e46869602c6444c47bfe4ba6c781084
Matteo Villa <matteo.villa@consulenti.chili.com>
2021-12-07 18:31:39 +0100

    extract function

--- extractFunction.py
+++ extractFunction.py
@@ -1,5 +1,17 @@
-def my_function():
-    print('ciao')
+def my_function(order: str):
+    data = order.split('-')
+    product = data[0]
+    price = data[1]
+
+    # format price
+    price_as_number = int(price)
+    formatted_price = f"{price_as_number:,}€"
+
+    # print order with price
+    stuff_in_string = f"product: {product}, price:{formatted_price}"
+    print("-----------")
+    print(stuff_in_string)
+    print("-----------")
 
 
-my_function()
+my_function('car-14000')
